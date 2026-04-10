730b6a856171223195431e0c8e7fb867899b7d30
wpa2 <wpa3@protonmail.com>
2026-04-10 15:05:25 +0100

    feat: rotate BT/USB IP display every 5 epochs

--- rust/plugins/ip_display.lua
+++ rust/plugins/ip_display.lua
@@ -1,10 +1,12 @@
 -- ip_display.lua: IP address display.
--- Shows BT IP when connected, falls back to USB IP.
+-- Rotates between BT IP and USB IP every 5 epochs when both available.
 plugin = {}
 plugin.name    = "ip_display"
-plugin.version = "3.0.1"
+plugin.version = "3.0.2"
 plugin.author  = "oxigotchi"
 plugin.tag     = "default"
+
+local tick = 0
 
 function on_load(config)
     register_indicator("ip_display", {
@@ -15,8 +17,13 @@ function on_load(config)
 end
 
 function on_epoch(state)
+    tick = tick + 1
     if state.bt_connected and state.bt_ip ~= "" then
-        set_indicator("ip_display", state.bt_ip)
+        if tick % 10 < 5 then
+            set_indicator("ip_display", state.bt_ip)
+        else
+            set_indicator("ip_display", state.display_ip)
+        end
     else
         set_indicator("ip_display", state.display_ip)
     end
